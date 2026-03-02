e591011bb69993fa38b3a8a01d5db3f3bfe42da7
PhanMaiHoaiNhi <hoainhi8999@gmail.com>
2026-03-02 14:37:47 +0700

    up baitap

---
+++ BaiTap4/Generic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap4
+{
+    internal class Generic
+    {
+        public static void display<T>(T value)
+        {
+            Console.WriteLine("Gia tri: " + value);
+        }
+
+        public static T traVe<T>(T value) {
+
+            return value;
+        }
+    }
+}
---
+++ BaiTap4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BaiTap4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Generic.display<int>(10);
+        Generic.display<string>("Hello World");
+        int kq = Generic.traVe<int>(20);
+        Console.WriteLine("Gia tri tra ve: " + kq);
+    }
+}
